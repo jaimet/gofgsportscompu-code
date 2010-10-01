df66a9264fec35ebabceac20da66d0bd442ccf22
viras <viras@671cf320-5fc3-452c-8009-8ee158a53c60>
2010-10-01 08:28:11 +0000

    - Cleaning up the trunk
    git-svn-id: https://gofgsportscompu.svn.sourceforge.net/svnroot/gofgsportscompu/trunk@44 671cf320-5fc3-452c-8009-8ee158a53c60

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include "GOFGSCMoblet.h"
-
-extern "C" int MAMain() {
-	Moblet::run(GOFGSCMoblet::Self() );
-	return 0;
-};
--- widgetLib/RadioGroup.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * RadioGroup.cpp
- *
- *  Created on: 06.09.2010
- *      Author: wkoller
- */
-
