434a1035435d679d1d3ded37f299160d945afb80
viras <viras@users.sourceforge.net>
2012-05-23 14:53:28 +0200

    Added base ("abstract") class for heartratemonitor handling Started ZephyrHxM implementation of HeartRateMonitor class

---
+++ js/hrm/heartratemonitor.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2012 Wolfgang Koller
+ *
+ * This file is part of GOFG Sports Computer - http://www.gofg.at/.
+ *
+ * GOFG Sports Computer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GOFG Sports Computer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GOFG Sports Computer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Base class for a heart-rate monitor
+ */
+function HeartRateMonitor() {
+}
+
+HeartRateMonitor.prototype.m_connectId = null;                  // Internal variable to handle the currently connected device
+HeartRateMonitor.prototype.m_HeartRateMonitorCallback = null;   // Storage for hrm-callback
+
+/**
+ * Get a list of available devices for the specific implementation
+ * p_successCallback gets passed a list of devices in the form:
+ * [ {id: id1, name: name1}, {id: id2, name: name2}, ... ]
+ * p_errorCallback gets passed an error message
+ */
+HeartRateMonitor.prototype.listDevices = function( p_successCallback, p_errorCallback ) {};
+
+/**
+ * Connect to a given device (and start tracking HeartRateMonitor data)
+ * p_deviceId should contain an ID of a device receive from listDevices before
+ * p_successCallback gets passed no argument
+ * p_errorCallback gets passed an error message
+ */
+HeartRateMonitor.prototype.connect = function( p_successCallback, p_errorCallback, p_deviceId ) {};
+
+/**
+ * Set the callback for new HeartRateMonitor data
+ * p_HeartRateMonitorCallback gets passed the new heartrate in beats per minute
+ */
+HeartRateMonitor.prototype.setCallback = function( p_HeartRateMonitorCallback ) {
+            this.m_HeartRateMonitorCallback = p_HeartRateMonitorCallback;
+        };
+
+/**
+ * Disconnect from a currently connected device
+ * NOTE: no argument passed, since the interface supports only one simultaneous connection
+ */
+HeartRateMonitor.prototype.disconnect = function() {};
