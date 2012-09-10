2236d0bdffe4f50ce608d6ebf3a8cfb1aa4450a1
viras <viras@users.sourceforge.net>
2012-09-10 11:37:09 +0200

    updates for translation & translator algorithm

--- js/translator.js
+++ js/translator.js
@@ -18,54 +18,62 @@
  */
 
 var Translator = {
-		m_contexts : new Array(),
-		ontranslate : null,
-		
-		register : function( p_context ) {
-			Translator.m_contexts.push( p_context );
-			
-			Translator._translate(p_context);
-		},
-		
-		changeLanguage : function( p_language ) {
-			// Load i18n module
-			$.i18n.properties( {
-				name: "gofgsc",
-				path: "i18n/",
-				mode: "map",
-				language: p_language,
-				callback: function() {
-					for( var i = 0; i < Translator.m_contexts.length; i++ ) {
-						Translator._translate( Translator.m_contexts[i] );
-					}
-					
-					// Setup jQuery mobile values
-                    $.mobile.loadingMessage = $.i18n.prop( "loading_message" );
-					
-					if( typeof Translator.ontranslate === "function" ) Translator.ontranslate();
+	m_contexts : new Array(),
+	ontranslate : null,
+
+	register : function(p_context) {
+		Translator.m_contexts.push(p_context);
+
+		Translator._translate(p_context);
+	},
+
+	changeLanguage : function(p_language) {
+		// Load i18n module
+		$.i18n.properties({
+			name : "gofgsc",
+			path : "i18n/",
+			mode : "map",
+			language : p_language,
+			callback : function() {
+				for ( var i = 0; i < Translator.m_contexts.length; i++) {
+					Translator._translate(Translator.m_contexts[i]);
 				}
-			} );
-		},
-		
-		/**
-		 * Apply translations to a given context
-		 * (called by the pagecreate events)
-		 */
-		_translate : function( p_context ) {
-			$(p_context).find( '*[data-i18n]' ).each( function() {
-				var trans = $.i18n.prop( $(this).attr( "data-i18n" ) );
-				findTextOnly.call( this );
-				
-				// Find the last children, required due to jQueryMobile adding elements
-				function findTextOnly() {
-					if( $(this).children().size() <= 0 ) {
-						$(this).text( trans );
-						return false;
-					}
-					else {
-						$(this).children().each( findTextOnly );
+
+				// Setup jQuery mobile values
+				$.mobile.loadingMessage = $.i18n.prop("loading_message");
+
+				if (typeof Translator.ontranslate === "function") Translator.ontranslate();
+			}
+		});
+	},
+
+	/**
+	 * Apply translations to a given context
+	 * (called by the pagecreate events)
+	 */
+	_translate : function(p_context) {
+		$(p_context).find('*[data-i18n]').each(function() {
+			var trans = $.i18n.prop($(this).attr("data-i18n"));
+			findTextOnly.call(this);
+
+			// Find the last children, required due to jQueryMobile adding elements
+			function findTextOnly() {
+				if ($(this).children().size() <= 0) {
+					// check for flip toggle switch
+					if ($(this).is('option') && $(this).parent('select[data-role="slider"]').length > 0) {
+						var flipToggleSwitch = $(this).parent('select[data-role="slider"]').next().first();
+						// this looks a bit crazy but basically just updates the corresponding span of the generated flipToggleSwitch
+						// the modulo 2 is there, because the order is inverted for the generated HTML-tags
+						flipToggleSwitch.children().eq(($(this).parent('select[data-role="slider"]').children().index($(this)) + 1) % 2).text(trans);
+					} else {
+						$(this).text(trans);
 					}
 				}
-			});
-		}
+				// .. continue searching if there are children left
+				else {
+					$(this).children().each(findTextOnly);
+				}
+			}
+		});
+	}
 };
