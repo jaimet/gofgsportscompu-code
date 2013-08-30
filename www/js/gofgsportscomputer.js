ebe1f1bdd75db52e84caa3481483f7b3c7c1646b
viras <viras@users.sourceforge.net>
2013-08-30 22:36:50 +0200

    custom style loading for windows phone version (due to init script running)

--- www/js/gofgsportscomputer.js
+++ www/js/gofgsportscomputer.js
@@ -103,13 +103,8 @@ var GOFGSportsComputer = {
 			GOFGSportsComputer._addStylesheet('lib/jquery.mobile/themes/android/AndroidHoloDarkLight.css', true);
 			GOFGSportsComputer._addStylesheet('lib/jquery.mobile/jquery.mobile.structure.css', true);
 			break;
-        case 'WinCE':
-        case 'Win32NT':
-			GOFGSportsComputer._addStylesheet('lib/jquery.mobile/themes/wp/jquery.mobile.wp.theme.css', true);
-			GOFGSportsComputer._addScript('lib/jquery.mobile/themes/wp/jquery.mobile.wp.theme.init.js', false);
-            break;
-        case 'iPhone':
-        default:
+                case 'iPhone':
+                default:
 			GOFGSportsComputer._addStylesheet('lib/jquery.mobile/jquery.mobile.structure.css', true);
 			GOFGSportsComputer._addStylesheet('lib/jquery.mobile/jquery.mobile.css', true);
 			break;
