312bd37614e70a4ce0a163941b7b61ef7e0d63b3
viras <viras@users.sourceforge.net>
2013-10-05 23:17:27 +0200

    fix formatting

--- www/js/pages/page.js
+++ www/js/pages/page.js
@@ -17,32 +17,32 @@
  * along with GOFG Sports Computer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if( pages == undefined ) {
-	var pages = {};
+if (pages == undefined) {
+    var pages = {};
 }
 
-function Page( p_name ) {
-	// Register page
-	pages[p_name] = this;
-	
-	// Keep name for later use
-	this.name = p_name;
-	
-	// Listen for the init & create event
-	$( '#' + p_name + '-page' ).live( 'pageinit', Utilities.getEvtHandler(this, this.init) );
-	$( '#' + p_name + '-page' ).live( 'pagecreate', Utilities.getEvtHandler(this, this.create) );
+function Page(p_name) {
+    // Register page
+    pages[p_name] = this;
 
-	// Listen to windows phone specific custom mouse events (used for simulating incomplete touch input)
-	$( '#' + p_name + '-page' ).live( 'wpmousedown', Utilities.getEvtHandler(this, this.wpmousedown) );
-	$( '#' + p_name + '-page' ).live( 'wpmouseup', Utilities.getEvtHandler(this, this.wpmouseup) );
-	$( '#' + p_name + '-page' ).live( 'wpmousemove', Utilities.getEvtHandler(this, this.wpmousemove) );
+    // Keep name for later use
+    this.name = p_name;
 
-	// Listen to gesture events
-	$( '#' + p_name + '-page' ).live( 'swipeleft', Utilities.getEvtHandler(this, this.swipeleft) );
-	$( '#' + p_name + '-page' ).live( 'swiperight', Utilities.getEvtHandler(this, this.swiperight) );
-	
-	// Listen to pagehide event
-	$( '#' + p_name + '-page' ).live( 'pagehide', Utilities.getEvtHandler(this, Page.addHistory, this) );
+    // Listen for the init & create event
+    $('#' + p_name + '-page').live('pageinit', Utilities.getEvtHandler(this, this.init));
+    $('#' + p_name + '-page').live('pagecreate', Utilities.getEvtHandler(this, this.create));
+
+    // Listen to windows phone specific custom mouse events (used for simulating incomplete touch input)
+    $('#' + p_name + '-page').live('wpmousedown', Utilities.getEvtHandler(this, this.wpmousedown));
+    $('#' + p_name + '-page').live('wpmouseup', Utilities.getEvtHandler(this, this.wpmouseup));
+    $('#' + p_name + '-page').live('wpmousemove', Utilities.getEvtHandler(this, this.wpmousemove));
+
+    // Listen to gesture events
+    $('#' + p_name + '-page').live('swipeleft', Utilities.getEvtHandler(this, this.swipeleft));
+    $('#' + p_name + '-page').live('swiperight', Utilities.getEvtHandler(this, this.swiperight));
+
+    // Listen to pagehide event
+    $('#' + p_name + '-page').live('pagehide', Utilities.getEvtHandler(this, Page.addHistory, this));
 }
 
 Page.prototype.leftPage = null;
@@ -58,37 +58,37 @@ Page.historyChangePage = false;
 /**
  * Add a page to the history stack
  */
-Page.addHistory = function( p_page ) {
-	if( !Page.historyChangePage && p_page.m_bInHistory ) {
-		Page.historyStack.push( p_page );
-	}
+Page.addHistory = function(p_page) {
+    if (!Page.historyChangePage && p_page.m_bInHistory) {
+        Page.historyStack.push(p_page);
+    }
 }
 
 /**
  * Go back one item in history stack
  */
 Page.backInHistory = function() {
-	if( Page.historyStack.length > 0 ) {
-		var page = Page.historyStack.pop();
+    if (Page.historyStack.length > 0) {
+        var page = Page.historyStack.pop();
 
-		// Switch back to page in history stack (but prevent from hashing page again)
-		Page.historyChangePage = true;
-		$.mobile.changePage( $('#' + page.name + '-page') );
-		Page.historyChangePage = false;
-	}
-	else {
-		if( device.platform.toLowerCase() == "android" ) {
-			// Exit app on android
-			var app = cordova.require("cordova/plugin/android/app");
-			if( app != null ) {
-				app.exitApp();
-			}
-		}
-		// Throw exception for windows phone
-		else {
-			throw "Last history item";
-		}
-	}
+        // Switch back to page in history stack (but prevent from hashing page again)
+        Page.historyChangePage = true;
+        $.mobile.changePage($('#' + page.name + '-page'));
+        Page.historyChangePage = false;
+    }
+    else {
+        if (device.platform.toLowerCase() == "android") {
+            // Exit app on android
+            var app = cordova.require("cordova/plugin/android/app");
+            if (app != null) {
+                app.exitApp();
+            }
+        }
+        // Throw exception for windows phone
+        else {
+            throw "Last history item";
+        }
+    }
 }
 
 /**
@@ -96,75 +96,79 @@ Page.backInHistory = function() {
  */
 Page.prototype.create = function() {
     // Register for translation
-    Translator.register( $('#' + this.name + '-page') );
-	
-    if( typeof this.oncreate === "function" ) this.oncreate();
+    Translator.register($('#' + this.name + '-page'));
+
+    if (typeof this.oncreate === "function")
+        this.oncreate();
 }
 
 /**
  * Automatically called when the page is inited
  */
 Page.prototype.init = function() {
-	// Register for translation
-	Translator.register( $('#' + this.name + '-page') );
-	
-    if( typeof this.oninit === "function" ) this.oninit();
+    // Register for translation
+    Translator.register($('#' + this.name + '-page'));
+
+    if (typeof this.oninit === "function")
+        this.oninit();
 }
 
 /**
-* WP7 event for supporting swipe gestures
-*/
+ * WP7 event for supporting swipe gestures
+ */
 Page.prototype.wpmousedown = function(evt) {
-	// Check if there is a page
-	if( this.leftPage != null || this.rightPage != null ) {
-		var mouse_evt = $.Event('mousedown', { originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY } );
-		$( '#' + this.name + '-page' ).trigger( mouse_evt );
-	}
+    // Check if there is a page
+    if (this.leftPage != null || this.rightPage != null) {
+        var mouse_evt = $.Event('mousedown', {originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY});
+        $('#' + this.name + '-page').trigger(mouse_evt);
+    }
 }
 
 /**
-* WP7 event for supporting swipe gestures
-*/
+ * WP7 event for supporting swipe gestures
+ */
 Page.prototype.wpmouseup = function(evt) {
-	// Check if there is a page
-	if( this.leftPage != null || this.rightPage != null ) {
-		var mouse_evt = $.Event('mouseup', { originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY } );
-		$( '#' + this.name + '-page' ).trigger( mouse_evt );
-	}
+    // Check if there is a page
+    if (this.leftPage != null || this.rightPage != null) {
+        var mouse_evt = $.Event('mouseup', {originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY});
+        $('#' + this.name + '-page').trigger(mouse_evt);
+    }
 }
- 
+
 /**
-* WP7 event for supporting swipe gestures
-*/
+ * WP7 event for supporting swipe gestures
+ */
 Page.prototype.wpmousemove = function(evt) {
-	// Check if there is a page
-	if( this.leftPage != null || this.rightPage != null ) {
-		var mouse_evt = $.Event('mousemove', { originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY } );
-		$( '#' + this.name + '-page' ).trigger( mouse_evt );
-	}
+    // Check if there is a page
+    if (this.leftPage != null || this.rightPage != null) {
+        var mouse_evt = $.Event('mousemove', {originalEvent: evt, pageX: evt.clientX, pageY: evt.clientY});
+        $('#' + this.name + '-page').trigger(mouse_evt);
+    }
 }
 
 /**
  * Called when the user swipes to the left
  */
 Page.prototype.swipeleft = function() {
-	// No this is not an error, but swiping to the left means making the right side visible
-	if( this.rightPage != null ) {
-		$.mobile.changePage( this.rightPage );
-	}
+    // No this is not an error, but swiping to the left means making the right side visible
+    if (this.rightPage != null) {
+        $.mobile.changePage(this.rightPage);
+    }
 }
 
 /**
  * Called when the user swipes to the right
  */
 Page.prototype.swiperight = function() {
-	// No this is not an error, but swiping to the right means making the left side visible
-	if( this.leftPage != null ) {
-		$.mobile.changePage( this.leftPage );
-	}
+    // No this is not an error, but swiping to the right means making the left side visible
+    if (this.leftPage != null) {
+        $.mobile.changePage(this.leftPage);
+    }
 }
 
-Page.prototype.getEvtHandler = function( p_callback ) {
-	var me = this;
-	return (function() { p_callback.call(me) } );
+Page.prototype.getEvtHandler = function(p_callback) {
+    var me = this;
+    return (function() {
+        p_callback.call(me)
+    });
 }
